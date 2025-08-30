0b4528018b8c81cc32a0b92fef65699dfcbf258a
Carmine Paolino <carmine@paolino.me>
2025-08-30 12:40:37 +0200

    Fix ActsAs module loading order in test environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,18 @@
 
 ENV['RAILS_ENV'] = 'test'
 
-require_relative 'dummy/config/environment'
+# Load Rails application but don't initialize yet
+require_relative 'dummy/config/application'
+
+# Ensure RubyLLM ActiveRecord integration is set up before models load
+ActiveSupport.on_load(:active_record) do
+  require 'ruby_llm/active_record/acts_as'
+  include RubyLLM::ActiveRecord::ActsAs
+end
+
+# Now initialize Rails (this will load models)
+Rails.application.initialize! unless Rails.application.initialized?
+
 require_relative 'spec_helper'
 
 # Ensure database is properly set up
@@ -15,9 +26,6 @@ end
 
 # Ensure schema is loaded for this spec file
 ActiveRecord::Tasks::DatabaseTasks.load_schema_current
-
-# Ensure RubyLLM ActiveRecord integration is included
-ActiveRecord::Base.include RubyLLM::ActiveRecord::ActsAs
 
 # Load models from JSON and save to database
 RubyLLM.models.load_from_json!
